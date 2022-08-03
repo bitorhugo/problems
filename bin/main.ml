9a6e4847ba38a81aa0a70a8cf86076154b59e2e1
Vitor <vitorhugosantos144@gmail.com>
2022-08-03 22:41:00 +0100

    problem two solved

--- bin/main.ml
+++ bin/main.ml
@@ -15,3 +15,6 @@ let last_int = One.last [0;1;2;3]
 
 let () = Printf.printf "last: %s\n" (string_of_stringoption last_string)
 let () = Printf.printf "last: %d\n" (int_of_intoption last_int)
+
+
+let last_two_string = Two.last_two ["a";"b";"c"]
